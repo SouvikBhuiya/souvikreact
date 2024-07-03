0eacd6027e2626dd5dfe18be3251a97ab5ee7edc
Souvik Bhuiya <web.souviksbhuiya@gmail.com>
2024-07-04 02:38:08 +0530

    remove router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from 'react';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from 'react-router-dom';
 
 
 function App() {
@@ -47,21 +47,21 @@ function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router> */}
          <Navbar title="TextSouvik" abouttext="About" mode={mode} togglemode={toggleMode} /> 
         <Alert alert={alert} />
         <div className="container my-3">
 
-          <Routes>
+          {/* <Routes>
             <Route path="/about"
               element={<About />} />
             <Route path="/"
               element={<TextForm showAlert={showAlert} heading="Enter the text to Analyze Below" mode={mode} />} />
-          </Routes>
-          {/* <TextForm showAlert={showAlert} heading="Enter the text to Analyze Below" mode={mode} /> */}
+          </Routes> */}
+          <TextForm showAlert={showAlert} heading="Enter the text to Analyze Below" mode={mode} />
           {/* <About /> */}
         </div>
-      </Router>
+      {/* </Router> */}
 
     </>
   );
